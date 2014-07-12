c94e7dd14bfcfe7c7389dacfd12f3af3586ad56a
Ole Rohne <ole.rohne@gmail.com>
2014-07-12 15:36:28 +0200

    Modified to rebase from git repository 	modified:   rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -1,6 +1,6 @@
 git stash
 git checkout master
-git svn rebase
+git pull --rebase origin
 git branch | grep / | while read b; do git rebase master $b; done
 git branch -D merged.old
 git branch -M merged merged.old
